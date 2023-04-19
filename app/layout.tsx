1d35019a3149d6669f9e6993dcec61e9f5ec1c83
25LUKE <EricLuke25@gmail.com>
2023-04-19 20:28:48 +0100

    Blog Website

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import './globals.css'
+import Navbar from './components/Navbar'
+import MyProfilePic from './components/MyProfilePic'
+
+export const metadata = {
+  title: "Eric's Blog",
+  description: 'Created by Eric Luke',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-slate-800">
+        <Navbar />
+        <MyProfilePic />
+        {children}
+      </body>
+    </html>
+  )
+}
